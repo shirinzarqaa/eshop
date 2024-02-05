7e07f4e7b75cbcdfb94486a49a7e7a7353d9421f
shirinzarqaa <shirinzarqaa354@gmail.com>
2024-02-05 22:14:46 +0700

    commit product repository Test

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -12,41 +12,41 @@ import java.util.List;
 
 @Repository
 public class ProductRepository {
-  private List<Product> productData = new ArrayList<>();
+    private List<Product> productData = new ArrayList<>();
 
 
-  public Product create(Product product) {
-    productData.add(product);
-    return product;
-  }
-
-  public void delete(String productId){
-    Product deletedProduct = this.findById(productId);
-    productData.remove(deletedProduct);
-  }
-
-  public Product edit(Product editedProduct) {
-    String editedProductId = editedProduct.getProductId();
-    Product productInRepository = this.findById(editedProductId);
-    int indexEditedProduct = productData.indexOf(productInRepository);
-    productData.set(indexEditedProduct, editedProduct);
-    return editedProduct;
-  }
-
-  public Iterator<Product> findAll() {
-    return productData.iterator();
-  }
-
-
-
-  public Product findById(String findProductId) {
-    Iterator<Product> productIterator = this.findAll();
-    while (productIterator.hasNext()) {
-      Product dataProduct = productIterator.next();
-      if (dataProduct.getProductId().equals(findProductId)) {
-        return dataProduct;
-      }
+    public Product create(Product product) {
+        productData.add(product);
+        return product;
     }
-    return null;
-  }
+
+    public void delete(String productId){
+        Product deletedProduct = this.findById(productId);
+        productData.remove(deletedProduct);
+    }
+
+    public Product edit(Product editedProduct) {
+        String editedProductId = editedProduct.getProductId();
+        Product productInRepository = this.findById(editedProductId);
+        int indexEditedProduct = productData.indexOf(productInRepository);
+        productData.set(indexEditedProduct, editedProduct);
+        return editedProduct;
+    }
+
+    public Iterator<Product> findAll() {
+        return productData.iterator();
+    }
+
+
+
+    public Product findById(String findProductId) {
+        Iterator<Product> productIterator = this.findAll();
+        while (productIterator.hasNext()) {
+            Product dataProduct = productIterator.next();
+            if (dataProduct.getProductId().equals(findProductId)) {
+                return dataProduct;
+            }
+        }
+        return null;
+    }
 }
---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -0,0 +1,73 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductRepositoryTest {
+
+    @InjectMocks
+    ProductRepository productRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testCreateAndFind() {
+        Product product = new Product();
+        product.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product.setProductName("Product 1");
+        product.setProductQuantity(100);
+        productRepository.create(product);
+
+        Iterator<Product> productIterator = productRepository.findAll();
+        assertTrue(productIterator.hasNext());
+
+        Product savedProduct = productIterator.next();
+        assertEquals(product.getProductId(), savedProduct.getProductId());
+        assertEquals(product.getProductName(), savedProduct.getProductName());
+        assertEquals(product.getProductQuantity(), savedProduct.getProductQuantity());
+    }
+
+    @Test
+    void testFindAllIfEmpty() {
+        Iterator<Product> productIterator = productRepository.findAll();
+        assertFalse(productIterator.hasNext());
+    }
+
+    @Test
+    void testFindAllIfMoreThanOneProduct() {
+        Product product1 = new Product();
+        product1.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product1.setProductName("Product 1");
+        product1.setProductQuantity(100);
+        productRepository.create(product1);
+
+        Product product2 = new Product();
+        product2.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd7");
+        product2.setProductName("Product 2");
+        product2.setProductQuantity(200);
+        productRepository.create(product2);
+
+        Iterator<Product> productIterator = productRepository.findAll();
+        assertTrue(productIterator.hasNext());
+
+        Product savedProduct = productIterator.next();
+        assertEquals(product1.getProductId(), savedProduct.getProductId());
+        savedProduct = productIterator.next();
+        assertEquals(product2.getProductId(), savedProduct.getProductId());
+        assertFalse(productIterator.hasNext());
+    }
+
+}
