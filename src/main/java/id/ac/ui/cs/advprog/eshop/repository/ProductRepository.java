9b2fee9638d234634cba87dff95a5a5d5c2997ad
shirinzarqaa <shirinzarqaa354@gmail.com>
2024-02-23 10:24:05 +0700

    push before solid

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -1,9 +1,8 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
 
-import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.model.*;
 import org.springframework.stereotype.Repository;
-
 
 import java.util.ArrayList;
 import java.util.Iterator;
