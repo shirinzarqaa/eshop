a67594f4b9bee7a85a598bfa05c77179ffe4713a
shirinzarqaa <shirinzarqaa354@gmail.com>
2024-03-06 06:07:48 +0700

    [RED] Add OrderRepository skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
@@ -44,7 +44,7 @@ class OrderRepositoryTest {
         Order order = orders.get(1);
         Order result = orderRepository.save(order);
 
-        Order findResult = orderRepository.findByid(orders.get(1).getId());
+        Order findResult = orderRepository.findById(orders.get(1).getId());
         assertEquals(order.getId(), result.getId());
         assertEquals(order.getId(),findResult.getId());
         assertEquals(order.getOrderTime(),findResult.getOrderTime());
@@ -77,6 +77,15 @@ class OrderRepositoryTest {
         assertEquals(orders.get(1).getOrderTime(), findResult.getOrderTime());
         assertEquals(orders.get(1).getAuthor(), findResult.getAuthor());
         assertEquals(orders.get(1).getStatus(), findResult.getStatus());
+    }
+
+    @Test
+    void testFindByIdIfIdNotFound() {
+        for (Order order : orders) {
+            orderRepository.save(order);
+        }
+        Order findResult = orderRepository.findById("zczc");
+        assertNull(findResult);
     }
 
     @Test
