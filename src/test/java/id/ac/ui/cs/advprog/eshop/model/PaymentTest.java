d81d71ed60791040b903a7a83a9c3cb2801b5acd
shirinzarqaa <shirinzarqaa354@gmail.com>
2024-03-06 17:29:17 +0700

    [RED] Make tests for Payment model

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -0,0 +1,75 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class PaymentTest {
+    private List<Order> orders;
+    private List<Product> products;
+
+    @BeforeEach
+    void setUp() {
+        this.orders = new ArrayList<>();
+        this.products = new ArrayList<>();
+
+        Product product1 = new Product();
+        product1.setId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product1.setName("Sampo Cap Bambang");
+        product1.setQuantity(2);
+        this.products.add(product1);
+
+        Product product2 = new Product();
+        product2.setId("a2c62328-4a37-4664-83c7-f32db8620155");
+        product2.setName("Sabun Cap Usep");
+        product2.setQuantity(1);
+        this.products.add(product2);
+
+        Order order1 = new Order("13652556-012a-4c07-b546-54eb1396d79b", this.products, 1708560000L, "Safira Sudrajat");
+        this.orders.add(order1);
+        Order order2 = new Order("65525631-a210-074c-45b6-54eb1396d79b", this.products, 1708570000L, "Bambang Bambang");
+        this.orders.add(order2);
+    }
+
+    @Test
+    void testCreatePaymentEmptyMethod() {
+        Map<String, String> paymentData = new HashMap<String, String>();
+        assertThrows(IllegalArgumentException.class, () -> {
+            Payment payment = new Payment("6c93d3e2-b009-46ba-9d15-f03d85adc2de", "", paymentData, this.orders.getFirst());});
+    }
+
+    @Test
+    void testCreatePaymentInvalidMethod() {
+        Map<String, String> paymentData = new HashMap<String, String>();
+        assertThrows(IllegalArgumentException.class, () -> {
+            Payment payment = new Payment("6c93d3e2-b009-46ba-9d15-f03d85adc2de", "PAYLATER", paymentData,
+                    this.orders.getFirst());
+        });
+    }
+
+    @Test
+    void testCreatePaymentEmptyPaymentData() {
+        Map<String, String> paymentData = new HashMap<String, String>();
+        assertThrows(IllegalArgumentException.class, () -> {
+            Payment payment = new Payment("6c93d3e2-b009-46ba-9d15-f03d85adc2de", "CASH_ON_DELIVERY", paymentData,
+                    this.orders.getFirst());
+        });
+    }
+
+    @Test
+    void testCreatePaymentEmptyOrder() {
+        Map<String, String> paymentData = new HashMap<String, String>();
+        paymentData.put("address", "Jalan Anggur");
+        paymentData.put("deliveryFee", "12000");
+        assertThrows(IllegalArgumentException.class, () -> {
+            Payment payment = new Payment("6c93d3e2-b009-46ba-9d15-f03d85adc2de", "CASH_ON_DELIVERY", paymentData,
+                    null);
+        });
+    }
+}
